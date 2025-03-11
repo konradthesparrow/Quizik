2d0db62d72cb5c7b7ffff0ea56d5c80438a85117
konradthesparrow <konradthesparrow@gmail.com>
2025-03-11 20:14:46 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-document.querySelector(".img1").setAttribute("src", "./images/dice" + randomNumber1 + ".png");
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-document.querySelector(".img2").setAttribute("src", "./images/dice" + randomNumber2 + ".png");
-
-if (randomNumber1 > randomNumber2)
-    document.querySelector("h1").innerText = document.querySelector("h1").innerText = "🚩Player 1 wins";
-else if (randomNumber1 < randomNumber2)
-    document.querySelector("h1").innerText = document.querySelector("h1").innerText = "Player 2 wins🚩";
-else
-document.querySelector("h1").innerText = document.querySelector("h1").innerText = "Draw!";
+const fs = require("fs");
+fs.writeFile("message.txt", "Hello Node", (err) => {
+  if (err) throw err;
+  console.log("The file has been saved!");
+});
